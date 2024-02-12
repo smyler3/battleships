798d12c951f0aa595546a5a8f5ddd7e0ffc578af
smyler3 <mylertalym01@gmail.com>
2024-02-12 16:29:13 +1100

    fix: allow using modules for webpack and jest
    jest now properly recognises ECMA modules allowing webpack to keep it's
    current syntax

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.m?js$": "babel-jest",
+    },
+    moduleNameMapper: {},
+    setupFilesAfterEnv: [],
+    moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
+};
