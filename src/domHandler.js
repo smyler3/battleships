1f0390d797eea421077ba6305c8ee5d703316446
smyler3 <mylertalym01@gmail.com>
2024-02-15 03:06:38 +1100

    feat: add board flipping and grid cell events
    the board can now be flipped on command, allowing visuals to match
    current player's POV in a real session, also added event listeners for
    clicking cells on enemy side

--- src/domHandler.js
+++ src/domHandler.js
@@ -1,8 +1,17 @@
-import { PLAYER_1_BOARD_ID, PLAYER_2_BOARD_ID } from "./constants";
+import { PLAYER_1_BOARD_ID, PLAYER_2_BOARD_ID, TILES } from "./constants";
 
 const createDOMHandler = () => {
+    let boardDisplay = null;
     let player1Board = null;
     let player2Board = null;
+
+    const selectCellEvent = (e) => {
+        const gridCell = e.target;
+
+        console.log(
+            `[${gridCell.getAttribute("data-x")}, ${gridCell.getAttribute("data-y")}]`,
+        );
+    };
 
     // Create a grid to store information about a player's board
     function createGridDisplay(grid, id) {
@@ -17,18 +26,42 @@ const createDOMHandler = () => {
                 gridCell.classList.add("grid-cell");
                 gridCell.setAttribute("data-x", x);
                 gridCell.setAttribute("data-y", y);
+                gridCell.setAttribute("data-player-id", id);
                 gridCell.textContent = cell;
 
                 board.appendChild(gridCell);
             });
         });
 
-        document.querySelector(".board-display").prepend(board);
+        boardDisplay.prepend(board);
+    }
+
+    function makeBoardClickable(board) {
+        Array.from(board.childNodes).forEach((cell) => {
+            if (
+                // Tile hasn't already been attacked
+                ![TILES.HIT, TILES.MISS].some(
+                    (tileType) => tileType === cell.textContent,
+                )
+            ) {
+                // Make selectable by click
+                cell.addEventListener("click", selectCellEvent);
+            }
+        });
+    }
+
+    function makeBoardUnclickable(board) {
+        Array.from(board.childNodes).forEach((cell) => {
+            // Remove selection by clicking
+            cell.removeEventListener("click", selectCellEvent);
+        });
     }
 
     return {
         // Create and render display of both players boards
         renderInitialBoard(player1Grid, player2Grid) {
+            boardDisplay = document.querySelector(".board-display");
+
             createGridDisplay(player1Grid, PLAYER_1_BOARD_ID);
             createGridDisplay(player2Grid, PLAYER_2_BOARD_ID);
 
@@ -38,6 +71,31 @@ const createDOMHandler = () => {
             // Position player 1's board facing screen
             player1Board.classList.add("focused-board");
             player2Board.classList.add("unfocused-board");
+
+            makeBoardClickable(player2Board);
+        },
+
+        // Flip the rendered board display
+        flipBoards() {
+            // Flip player 1 board cells
+            player1Board.classList.toggle("focused-board");
+            player1Board.classList.toggle("unfocused-board");
+
+            // Flip player 2 board cells
+            player2Board.classList.toggle("focused-board");
+            player2Board.classList.toggle("unfocused-board");
+
+            // Switch board positions
+            boardDisplay.prepend(boardDisplay.lastChild);
+
+            // Make defender board clickable for providing attacks
+            if (boardDisplay.firstChild === player1Board) {
+                makeBoardClickable(player1Board);
+                makeBoardUnclickable(player2Board);
+            } else {
+                makeBoardClickable(player2Board);
+                makeBoardUnclickable(player1Board);
+            }
         },
     };
 };
